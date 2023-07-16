e9c2fce58786908171e6a35633ffc33f8e0d81ed
sonofevil <debilerpc@web.de>
2023-07-16 15:06:50 +0200

    Create linktree.bat

---
+++ batch/linktree.bat
@@ -0,0 +1,24 @@
+@echo off
+
+echo Usage: linktree.bat [sourcepath] [targetpath]
+echo Creates a copy of [sourcepath]'s directory structure 
+echo at [targetpath] but with symbolic links to files.
+echo:
+
+if "%1"=="" exit /b
+if "%2"=="" echo No target specified. && exit /b
+
+cd /d "%~1"
+if ERRORLEVEL 1 echo Not a valid source directory. && exit /b
+set "targetDir=%~2"
+IF %targetDir:~-1%==\ SET targetDir=%targetDir:~0,-1%
+
+setlocal enableextensions
+setlocal enabledelayedexpansion
+for /R . %%f in (*) do (
+  set absoPath=%%f
+  set relaPath=!absoPath:%CD%\=!
+  set linkPath=%targetDir%\!relaPath!
+  mkdir "!linkpath!\.."
+  mklink "!linkPath!" "!absoPath!"
+)
